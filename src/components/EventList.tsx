89e43f3f0af6b83071e6cd79a47258088581c011
Deniss B <zoomyman@gmail.com>
2024-12-02 20:42:06 +0000

    tag filter toggle button fix

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useReducer, useTransition, useCallback, useMemo } from 'react'
-import { range } from 'lodash'
 import data from '../data/data.json'
 import { Event } from '../schema'
 import { TagFiltersSection } from '../components/tag-filters/TagFilters'
@@ -7,13 +6,15 @@ import { eventsFilter } from '../eventsFilter'
 import { TextSearch } from '../components/text-search/TextSearch'
 import RadioGroup from '../components/radio-group/RadioGroup'
 import Listing from '../components/listing/Listing'
+import { PaginationNavigation, NavigationState } from '../components/pagination/PaginationNavigation'
+import { produce } from 'immer'
 
 class EventListState {
   remoteData: Event[] = []
   searchInput: string = ""
   typeSwitch: string = "All"
   selectedTags: string[] = []
-  pagination: { skip: number, limit: number } = { skip: 0, limit: 10 }
+  pagination: NavigationState = new NavigationState()
 }
 
 type Action =
@@ -21,23 +22,26 @@ type Action =
   | { type: 'updateSearchInput', payload: string }
   | { type: 'updateRadioGoupValue', payload: string }
   | { type: 'updateSelectedTags', payload: string[] }
-
+  | { type: 'updatePagination', payload: NavigationState}
 
 function reducer(state: EventListState, action: Action): EventListState {
   switch (action.type) {
     case 'loadEvents':
-      // return { ...state, remoteData: action.payload }
-      return Object.assign({}, state, { remoteData: action.payload })
+      return { ...state, remoteData: action.payload }
     case 'updateSearchInput':
-      // return { ...state, searchInput: action.payload }
-      console.log(action.payload)
       return Object.assign({}, state, { searchInput: action.payload })
     case 'updateRadioGoupValue':
-      // return { ...state, typeSwitch: action.payload }
-      return Object.assign({}, state, { typeSwitch: action.payload })
+      return { ...state, typeSwitch: action.payload }
     case 'updateSelectedTags':
-      // return { ...state, selectedTags: action.payload }
       return Object.assign({}, state, { selectedTags: action.payload })
+    case 'updatePagination':
+      let skip = action.payload.skip
+      if(action.payload.skip > state.remoteData.length){
+        skip = Math.floor(state.remoteData.length / state.pagination.limit) * state.pagination.limit
+      }
+      return produce(state, (draft) => {
+        draft.pagination = action.payload
+      })
     default:
       return state
   }
@@ -49,9 +53,7 @@ export const EventList = () => {
 
   const [filtersVisible, setFiltersVisible] = useState(false)
   const [dataFetched, setDataFetched] = useState<boolean>(false)
-  const [skip, setSkip] = useState<number>(0)
-  const [limit, setLimit] = useState<number>(10)
-
+  const [skip, setSkip] = useState(0)
 
   const filteredEvents = eventsFilter(state.remoteData, {
     searchTerm: state.searchInput,
@@ -59,12 +61,12 @@ export const EventList = () => {
     tags: state.selectedTags
   })
 
-  if (skip > filteredEvents.length) {
-    setSkip(Math.floor(filteredEvents.length / limit) * limit)
+  if (state.pagination.skip > filteredEvents.length) {
+    setSkip(Math.floor(filteredEvents.length / state.pagination.limit) * state.pagination.limit)
   }
 
-  const totalPages = Math.ceil(filteredEvents.length / limit)
-  const currentPage = skip / limit + 1
+  // const totalPages = Math.ceil(filteredEvents.length / state.pagination.limit)
+  // const currentPage = state.pagination.skip / state.pagination.limit + 1
 
   useEffect(() => {
     startTransition(() => {
@@ -79,6 +81,7 @@ export const EventList = () => {
   const onInputChange = useCallback((newTextValue: string) => dispatch({ type: 'updateSearchInput', payload: newTextValue }), [])
   const onRadioChange = useCallback((newValue: string) => dispatch({ type: "updateRadioGoupValue", payload: newValue }), [])
   const onTagsChange = useCallback((selectedTags: string[]) => dispatch({ type: "updateSelectedTags", payload: selectedTags }), [])
+  const onPaginationChange = useCallback((newPagination: NavigationState) => dispatch({type: 'updatePagination', payload: newPagination}), [])
 
   return (
     <div className="app events">
@@ -89,59 +92,20 @@ export const EventList = () => {
       </div>
 
       <div className="events_tag-filter">
-        <input type="button" tabIndex={0} onClick={() => setFiltersVisible(filtersVisible ? false : true)} value={filtersVisible ? `open filters` : `close filters`} /> <span className="events_tag-counter">{state.selectedTags.length ? state.selectedTags.length : `...`}</span>
+        <input type="button" tabIndex={0} onClick={() => setFiltersVisible(filtersVisible ? false : true)} value={filtersVisible ? `close filters` : `open filters`} /> <span className="events_tag-counter">{state.selectedTags.length ? state.selectedTags.length : `...`}</span>
         {filtersVisible && <TagFiltersSection tags={data.tagFilters} selected={state.selectedTags} onChange={onTagsChange} />}
       </div>
 
       {!isPending ?
-        <Listing eventsData={{ eventCardList: filteredEvents.slice(skip, skip + limit) }} />
+        <Listing eventsData={{ eventCardList: filteredEvents.slice(state.pagination.skip, state.pagination.skip + state.pagination.limit) }} />
         :
         "Loading data..." // this does not work
       }
 
-      {filteredEvents.length > limit && (
-        <div className="pagination-navigation">
-          <button
-            disabled={skip <= 0}
-            onClick={() => setSkip(skip - limit)}
-          >
-            Prev page
-          </button>
-
-          {/* Always show the first page */}
-          {currentPage > 1 && (
-            <>
-              <button onClick={() => setSkip(0)}>1</button>
-              <button disabled>...</button>
-            </>
-          )}
-
-          {/* Show a range of pages around the current page */}
-          {range(Math.max(currentPage - 1, 0), Math.min(currentPage + 1, totalPages)).map(i => (
-            <button
-              key={i}
-              disabled={i + 1 === currentPage}
-              onClick={() => setSkip(i * limit)}
-            >
-              {i + 1}
-            </button>
-          ))}
-
-          {/* Always show the last page */}
-          {currentPage < totalPages - 2 && (
-            <>
-              <button disabled>...</button>
-              <button onClick={() => setSkip((totalPages - 1) * limit)}>{totalPages}</button>
-            </>
-          )}
-
-          <button
-            disabled={(skip + limit) >= filteredEvents.length}
-            onClick={() => setSkip(skip + limit)} >
-            Next page
-          </button>
-        </div>
+      {filteredEvents.length > state.pagination.limit && (
+        <PaginationNavigation pagination={state.pagination} totalObjects={ filteredEvents.length } onChange={onPaginationChange} />
       )}
+
     </div>
   )
 }
